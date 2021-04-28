6fccbd166035c3ed5dda186ed3876240ad1d280e
EverlaststingRolich <60112075+EverlaststingRolich@users.noreply.github.com>
2021-04-28 16:02:04 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+#ede
