4f24241f266fe7e31b65ca46a1dbd491e265eeb0
EverlaststingRolich <60112075+EverlaststingRolich@users.noreply.github.com>
2021-04-28 16:15:32 +0300

    first commit

--- bot.py
+++ bot.py
@@ -11,3 +11,6 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+#test
